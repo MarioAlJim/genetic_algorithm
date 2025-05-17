4ff5f8846ffdce44467e9615be660f48e4285d1b
One-Liu <lesterhero2002@gmail.com>
2025-05-16 19:46:54 -0600

    FIX: Fixed import and dropped unused attributes.

--- src/models/algorithm.py
+++ src/models/algorithm.py
@@ -1,7 +1,7 @@
 """This file is for defining the algorithm class.
 
 The algorithm class is the base class for all algorithms."""
-from src.models.evaluation import TriangleClassification
+from src.models.evaluations.triangle_classification import TriangleClassification
 
 class Algorithm:
     """Algorithm base class"""
@@ -11,7 +11,6 @@ class Algorithm:
             ['triangle-classification', TriangleClassification],
         ]
         self._evaluation = TriangleClassification()
-        self._expected_solution = 'scalene'
 
     @property
     def name(self) -> str:
@@ -32,18 +31,3 @@ class Algorithm:
                 return
 
         raise ValueError('Evaluation must be a valid value: ', self._evaluations)
-
-    @property
-    def expected_solution(self) -> str:
-        """Get expected solution"""
-        return self._expected_solution
-
-    @expected_solution.setter
-    def expected_solution(self, expected_solution) -> None:
-        """Set expected solution"""
-        expected_solutions = self._evaluation.expected_solutions
-
-        if expected_solution not in expected_solutions:
-            raise ValueError('Expected solution must be: ', expected_solutions)
-
-        self._expected_solution = expected_solution
