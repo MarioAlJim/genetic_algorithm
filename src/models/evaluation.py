455120dcc54afbbd66bd9db7e0a6e4a24710c378
One-Liu <lesterhero2002@gmail.com>
2025-05-16 19:45:19 -0600

    FIX: Changed the implementation of the evaluation.

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -1,10 +1,13 @@
-"""This file is for defining the evaluations."""
+"""This file is for defining the evaluation class."""
+import os
+import coverage
 
 class Evaluation:
     """Evaluation class"""
     def __init__(self) -> None:
         self._name = 'Evaluation'
-        self._expected_solutions = []
+        self._branches = 0
+        self._inputs = 0
 
     @property
     def name(self) -> str:
@@ -12,71 +15,37 @@ class Evaluation:
         return self._name
 
     @property
-    def expected_solutions(self) -> list:
-        """Get expected solutions"""
-        return self._expected_solutions
+    def branches(self) -> int:
+        """Get number of branches"""
+        return self._branches
 
-    def score(self, data: list, expected_solution: str) -> list:
+    @property
+    def inputs(self) -> int:
+        """Get number of inputs"""
+        return self._inputs
+
+    def _get_coverage(self, target_function, suite: list) -> float:
+        """Measures the code coverage of the target function given a suite of inputs."""
+        coverage_percent = 0
+        config_file = os.environ['COVERAGE_CONFIG']
+        cov = coverage.Coverage(config_file=config_file)
+
+        with cov.collect():
+            it = iter(suite)
+            # Use zip to group the inputs from the suite and pass them to the target function
+            for args in zip(*[it] * self.inputs):
+                target_function(args)
+
+        for filename in cov.get_data().measured_files():
+            analysis = cov.analysis2(filename)
+            executable_statements = len(analysis[1])
+            statements_not_run = len(analysis[3])
+            statements_run = executable_statements - statements_not_run
+
+            coverage_percent = round(statements_run * 100 / executable_statements, 2)
+
+        return coverage_percent
+
+    def score(self, suite: list) -> list:
         """Evaluation method"""
         raise NotImplementedError('Evaluation method should be implemented by child class')
-
-class TriangleClassification(Evaluation):
-    """Triangle classification class"""
-    def __init__(self) -> None:
-        super().__init__()
-        self._name = 'triangle-classification'
-        self._expected_solutions = [
-            'scalene',
-            'equilateral',
-            'isosceles',
-            'invalid',
-            'out of range'
-        ]
-
-    def score(self, data: list, expected_solution: str) -> list:
-        """Evaluates the generated data
-
-        I.E.
-        Expected solution input: equilateral
-        Input: [3,3,3]
-        Output: [[3,3,3], 0]
-        """
-        # Classify the triangle
-        classification = self.classify_triangle(data)
-
-        # Score the chromosome
-        score = 0
-
-        if classification == expected_solution:
-            score = 1
-
-        return [data, score]
-
-    @staticmethod
-    def classify_triangle(data: list) -> str:
-        """
-        Classify the triangle based on the given data
-        Input: [a, b, c]
-        Output: 'scalene', 'equilateral', 'isosceles', 'invalid', 'out of range'
-        """
-        classification = "out of range"
-        a = data[0]
-        b = data[1]
-        c = data[2]
-
-        if a > 0 and b > 0 and c > 0:
-            if a + b > c and b + c > a and c + a > b:
-                if a != b and b != c and c != a:
-                    classification = 'scalene'
-                elif a == b == c:
-                    classification = 'equilateral'
-                elif ((a == b != c) or
-                      (a == c != b) or
-                      (b == c != a)):
-                    classification = 'isosceles'
-            else:
-                classification = 'invalid'
-        else:
-            classification = 'out of range'
-
-        return classification
