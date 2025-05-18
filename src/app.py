7541464792ac58dc6e58a4ca669668a358f9fad1
Mario Alberto Jimenez Jimenez <zs20015742@estudiantes.uv.mx>
2025-05-18 17:05:12 -0600

    FIX: Temporary file cleaner added

--- src/app.py
+++ src/app.py
@@ -1,10 +1,22 @@
 """This file is for executing the app"""
 # Third party imports
+import os
 from flask import Flask, redirect, url_for
 
 # Local application imports
 from routes.about_routes import about_blueprint
 from routes.playground_routes import playground_blueprint
+
+def clean_temp_files(directory, extensions):
+    for filename in os.listdir(directory):
+        if any(filename.endswith(ext) for ext in extensions):
+            file_path = os.path.join(directory, filename)
+            try:
+                os.remove(file_path)
+                print(f"Archivo eliminado: {file_path}")
+            except Exception as e:
+                print(f"No se pudo eliminar {file_path}: {e}")
+
 
 def create_app() -> Flask:
     """Create and set up the app"""
@@ -16,10 +28,11 @@ def create_app() -> Flask:
 
     @new_app.route('/')
     def home():
-        return redirect(url_for('playground_blueprint.show_ga_playground'))
+        return redirect(url_for('playground_blueprint.show_playground'))
 
     return new_app
 
 if __name__ == '__main__':
+    clean_temp_files("routes", [".json"])
     app = create_app()
-    app.run(host='localhost', port=3000, debug=True)
+    app.run(host='192.168.1.103', port=3000, debug=True)
