fe4e7f2a66088d3584899675522ca62b4276a388
One-Liu <lesterhero2002@gmail.com>
2025-06-11 09:52:32 -0600

    FEAT: Add show notification. Only used when the execution file does not exist anymore.

--- src/static/js/scripts.js
+++ src/static/js/scripts.js
@@ -23,3 +23,30 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+function showNotification(message, type='info') {
+    const container = document.getElementById("notification-container");
+    const notification = document.createElement("div");
+    notification.className = `notification--${type}`;
+    notification.classList.add('notification');
+    notification.textContent = message;
+
+    container.appendChild(notification);
+    void notification.offsetWidth;
+    notification.classList.add('notification--show');
+
+    const autoHideTimer = setTimeout(() => {
+        hideNotification(notification);
+    }, 5000);
+
+    notification.addEventListener('click', () => {
+        clearTimeout(autoHideTimer);
+        hideNotification(notification);
+    });
+}
+
+function hideNotification(notification) {
+    notification.classList.remove('notification--show');
+    setTimeout(() => {
+        notification.remove();
+    }, 300);
+}
